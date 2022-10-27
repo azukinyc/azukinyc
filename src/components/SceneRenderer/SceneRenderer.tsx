2185159939d4f7f79981490a09bf80e48ebbd243
ArielSaldana <hello@ariel.io>
2022-10-26 21:06:30 -0400

    Clean up code

--- src/components/SceneRenderer/SceneRenderer.tsx
+++ src/components/SceneRenderer/SceneRenderer.tsx
@@ -1,15 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import bean from '../../assets/images/bean-7783.png'
 import beanJson from "../../assets/json/beans.json"
 
 function SceneRenderer() {
-
     function getRandomInt(max: number) {
         return Math.floor(Math.random() * max);
     }
 
     const [pageLoadCount, setPageLoadCount] = useState(0)
-    const [imgSrc, setImgSrc] = useState("")
+    const [beanImgSrc, setBeanImgSrc] = useState("")
     const [beanId, setBeanId] = useState(0)
 
     useEffect(() => {
@@ -21,10 +19,8 @@ function SceneRenderer() {
             const beanProfile = beanJson[rand];
 
             changeBackgroundColor(beanProfile.background.hex)
-            setBeanImageSrc(`../../assets/images/final-${beanProfile.id}.png`)
             setBeanId(beanProfile.id)
-            console.log(beanProfile);
-
+            setBeanImgSrc(`../../assets/images/final-${beanProfile.id}.png`)
         }
     })
 
@@ -32,15 +28,11 @@ function SceneRenderer() {
         document.body.style.backgroundColor = color
     }
 
-    function setBeanImageSrc(imageSrc: string): void {
-        setImgSrc(imageSrc)
-    }
-
     const beanAltText = `Azuki Bean ${beanId}`
 
     return (
             <div className="image-wrapper">
-                <img src={imgSrc} alt={beanAltText}/>
+                <img src={beanImgSrc} alt={beanAltText}/>
             </div>
             );
 }
