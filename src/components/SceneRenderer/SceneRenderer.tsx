de3353a479ac076a266632ca547691fecd1e565a
ArielSaldana <hello@ariel.io>
2022-10-27 09:10:06 -0400

    Smooth loading animations

--- src/components/SceneRenderer/SceneRenderer.tsx
+++ src/components/SceneRenderer/SceneRenderer.tsx
@@ -1,14 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import beanJson from "../../assets/json/beans.json"
+import beanJson from "../../assets/json/beans.json";
+import './SceneRenderer.css';
 
 function SceneRenderer() {
     function getRandomInt(max: number) {
         return Math.floor(Math.random() * max);
     }
 
-    const [pageLoadCount, setPageLoadCount] = useState(0)
-    const [beanImgSrc, setBeanImgSrc] = useState("")
-    const [beanId, setBeanId] = useState(0)
+    const [pageLoadCount, setPageLoadCount] = useState(0);
+    const [bean, setBean] = useState(Object);
 
     useEffect(() => {
         if (pageLoadCount === 0) {
@@ -17,22 +17,31 @@ function SceneRenderer() {
             const beansListLength = beanJson.length;
             const rand = getRandomInt(beansListLength);
             const beanProfile = beanJson[rand];
-
-            changeBackgroundColor(beanProfile.background.hex)
-            setBeanId(beanProfile.id)
-            setBeanImgSrc(`../../assets/images/final-${beanProfile.id}.png`)
+            setBean(beanProfile);
+            changeBackgroundColor(beanProfile.background.hex);
+            fadeInBean();
         }
     })
 
     function changeBackgroundColor(color: string): void {
-        document.body.style.backgroundColor = color
+        document.body.style.backgroundColor = color;
     }
 
-    const beanAltText = `Azuki Bean ${beanId}`
+    function fadeInBean(): void {
+        setTimeout(() => {
+            const elem = document.getElementById("bean-img")
+            if (elem) {
+                elem.style.opacity = "1";
+            }
+        }, 300)
+    }
+
+    const beanAltText = `Azuki Bean ${bean.id}`;
+    const beanImageSrc = `../../assets/images/final-${bean.id}.png`;
 
     return (
             <div className="image-wrapper">
-                <img src={beanImgSrc} alt={beanAltText}/>
+                <img id="bean-img" src={beanImageSrc} alt={beanAltText}/>
             </div>
             );
 }
