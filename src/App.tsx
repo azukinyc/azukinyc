3abea7fc7e47290ce3ff2ee08353833088d005a3
ArielSaldana <hello@ariel.io>
2023-01-25 19:22:57 -0500

    Refactor to use @unreal/unreal-components cursor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css';
+import '@unreal/unreal-components/dist/style.css'
 import Navigation from "./components/Navigation/Navigation";
 import BeansHero from "./components/Hero/BeansHero";
-import Cursor from "./components/Mouse/Cursor";
+import { Cursor } from "@unreal/unreal-components"
+import bean from './assets/svgs/bean.svg'
+
 
 function App() {
   return (
       <div className="App">
-          <Cursor />
+          <Cursor hexColor={'rgba(0,0,0,0)'} size={20} sizeLarge={40} svg={bean} />
           <div className="">
               <BeansHero/>
               <div className="navigation-positioner">
