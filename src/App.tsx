abf042d71939e0e7e22db2ce893ccb86d5bf0b13
ArielSaldana <hello@ariel.io>
2023-01-25 21:43:08 -0500

    Use @unreal/pan Detector to detect if device is mobile or touchscreen. If either, disable the custom cursor.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css';
 import '@unreal/unreal-components/dist/style.css'
+import { Detector } from "@unreal/pan";
 import Navigation from "./components/Navigation/Navigation";
 import BeansHero from "./components/Hero/BeansHero";
 import { Cursor } from "@unreal/unreal-components"
 import bean from './assets/svgs/bean.svg'
 
-
 function App() {
+    const [customCursorEnabled, setCustomCursorEnabled] = useState(false)
+    useEffect(() => {
+        const detector = Detector.getInstance()
+        if (detector !== undefined) {
+            setCustomCursorEnabled(!detector.isTouchScreen() && !detector.isMobile())
+        }
+    }, [customCursorEnabled])
   return (
       <div className="App">
-          <Cursor hexColor={'rgba(0,0,0,0)'} size={20} sizeLarge={40} svg={bean} />
+          {customCursorEnabled &&
+            <Cursor hexColor={'rgba(0,0,0,0)'} size={20} sizeLarge={40} svg={bean} />
+          }
           <div className="">
               <BeansHero/>
               <div className="navigation-positioner">
