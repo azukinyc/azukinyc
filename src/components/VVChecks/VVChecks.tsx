c14bb22b02484a13857ee47ca3fc0b2cc1b0115d
ArielSaldana <hello@ariel.io>
2023-02-07 23:31:30 -0500

    fade in beanz onLoad

--- src/components/VVChecks/VVChecks.tsx
+++ src/components/VVChecks/VVChecks.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import check from '../../assets/svgs/checks.svg'
 import beanSelfie from '../../assets/images/final-7132.png'
 import styles from './VVChecks.module.css'
 
- const url = (id: number): string => {
+const url = (id: number): string => {
     //https://azkimg.imgix.net/images_squareface/final-14989.png?fm=jpg&w=1536
     return 'https://azkimg.imgix.net/images_squareface/final-' + id + '.png?fm=jpg&w=40'
 }
@@ -13,24 +13,50 @@ function randomIntFromInterval(min: number, max: number) {
 }
 
 const rndInt = randomIntFromInterval(1, 6)
+
 function VVChecks() {
 
     const arr: string[] = []
 
     for (let i = 0; i < 80; i++) {
-        arr.push(url(randomIntFromInterval(0,20000)))
+        arr.push(url(randomIntFromInterval(0, 20000)))
     }
 
     console.log(arr)
+
+//    useEffect(() => {
+//        var img = document.querySelector('img')
+//
+//        function loaded() {
+//            console.log('loaded')
+//        }
+//
+//        if (img && img.complete) {
+//            loaded()
+//        } else {
+//            img!.addEventListener('load', loaded)
+//            img!.addEventListener('error', function () {
+//                alert('error')
+//            })
+//        }
+//    }, [])
+
+    const log = (elem: HTMLElement) => {
+        elem.style.opacity = '1'
+    }
 
     return (
         <div className={styles.vvchecks}>
             <div className={styles.frame}>
                 <div className={styles.canvas}>
                     {[...Array(80)].map((x, i) =>
-                    <div key={i} className={styles.cell}>
-                        <img src={arr[i]} className="" alt="check"/>
-                    </div>
+                        <div key={i} className={styles.cell}>
+                            <img
+                                src={arr[i]}
+                                onLoad={ (event) => log(event.target as HTMLElement) }
+                                className=""
+                                alt="check"/>
+                        </div>
                     )}
 
                 </div>
