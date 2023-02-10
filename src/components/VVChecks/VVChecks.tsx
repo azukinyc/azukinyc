bc98cc5f1ce0a6b7f3ad4d63f9b1154500465a31
ArielSaldana <hello@ariel.io>
2023-02-09 19:25:16 -0500

    Changed component to use local version of beanz.

--- src/components/VVChecks/VVChecks.tsx
+++ src/components/VVChecks/VVChecks.tsx
@@ -1,18 +1,13 @@
 import React, {useEffect} from 'react';
-import check from '../../assets/svgs/checks.svg'
-import beanSelfie from '../../assets/images/final-7132.png'
 import styles from './VVChecks.module.css'
 
 const url = (id: number): string => {
-    //https://azkimg.imgix.net/images_squareface/final-14989.png?fm=jpg&w=1536
-    return 'https://azkimg.imgix.net/images_squareface/final-' + id + '.png?fm=jpg&w=40'
+    return '/assets/images/tinybeans/final-' + id + '.png'
 }
 
 function randomIntFromInterval(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
-const rndInt = randomIntFromInterval(1, 6)
 
 function VVChecks() {
     const arr: string[] = []
